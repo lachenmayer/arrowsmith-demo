a2f8cd304bccb9f581c3e3d93567efdd5e0eaaa3
Harry Lachenmayer <harrylachenmayer@gmail.com>
2015-06-21 20:43:28 +0100

    demo -> clock

--- Clock.elm
+++ Clock.elm
@@ -1,19 +1,9 @@
-module Demo where
+module Clock where
 
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Time exposing (..)
-
-answer = 42
-
-otherAnswer = 1337
-
-betterAnswer =
-  bigger answer otherAnswer
-
-bigger first second =
-  if first > second then first else second
 
 redCircle =
   filled red (circle 150)
